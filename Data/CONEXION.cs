49fcf538f18f80ffcc8680da1d1bd70fc70a8ce2
Smerlyn A. García Valenciano <78616922+Nylrems@users.noreply.github.com>
2022-05-09 23:44:45 -0400

    Agregando clase para la conexión (incompleta)

---
+++ Data/CONEXION.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsuariosApp_CSharp.Data
+{
+    internal class CONEXION
+    {
+    }
+}
