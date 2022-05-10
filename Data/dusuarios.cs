3c077121efad2bdfba5e68797366a25faeba7da4
Smerlyn A. García Valenciano <78616922+Nylrems@users.noreply.github.com>
2022-05-09 23:33:24 -0400

    Agregando datos

---
+++ Data/dusuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using UsuariosApp_CSharp.Logic;
+
+namespace UsuariosApp_CSharp.Data
+{
+    class dusuarios
+    {
+        private SqlCommand cmd = new SqlCommand();
+        private int idusuario;
+
+        public bool insertar(lusuarios dt)
+        {
+
+        }
+    }
+}
